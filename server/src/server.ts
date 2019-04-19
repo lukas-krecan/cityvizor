68103d28dc9d90a98d93b3b11db73fd7d5d24381
lukas-krecan <lukas@krecan.net>
2019-04-19 09:42:26 +0200

    Support for TypeScript on server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,13 @@
 console.log("Starting CityVizor Server");
 console.log("Node version: " + process.version);
 
-var http = require('http');
-var express = require('express');
+const http = require('http');
+const express = require('express');
 
-var config = require("../config");
+const config = require("../config");
 
 /* SET UP ROUTING */
-var app = express();
+const app = express();
 
 /* CORS FOR DEVELOPMENT */
 if(config.cors.enabled){
@@ -20,12 +20,12 @@ if(config.cors.enabled){
 require('express-async-errors');
 
 if (config.server.compression) {
-	var compression = require('compression');
+	const compression = require('compression');
 	app.use(compression());
 }
 
 // parse body
-var bodyParser = require("body-parser");
+const bodyParser = require("body-parser");
 app.use(bodyParser.json({})); // support json encoded bodies
 app.use(bodyParser.urlencoded({
 	extended: true,
@@ -39,12 +39,12 @@ require("./file-storage");
 require("./db");
 
 /* AUTHENTICATION */
-var jwt = require('express-jwt');
+const jwt = require('express-jwt');
 app.use(jwt(config.jwt));
 
 /* ACCESS CONTROL */
-var acl = require("express-dynacl");
-var aclOptions = {
+const acl = require("express-dynacl");
+const aclOptions = {
 	roles: {
 		"guest": require("./acl/guest"),
 		"user": require("./acl/user"),
@@ -55,7 +55,7 @@ var aclOptions = {
 	defaultRoles: ["guest"],
 	userRoles: ["user"],
 	logConsole: true
-}
+};
 acl.config(aclOptions);
 
 /* ROUTING */
@@ -66,9 +66,9 @@ app.use(require("./middleware/error-handler"));
 
 
 /* SET UP SERVER */
-let host = config.server.host || "127.0.0.1";
-let port = config.server.port || 80;
+const host = config.server.host || "127.0.0.1";
+const port = config.server.port || 80;
 
-http.createServer(app).listen(port, host, function() {
+http.createServer(app).listen(port, host, () => {
 	console.log('[SERVER] Listening on ' + host + ':' + port + '!');
 });
